7f7cf98abcefe5b96a4361b1ed31e7a864724ff1
Felipe Vogt <felipevogt.f@gmail.com>
2023-03-20 01:22:50 -0300

    ajuste variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const HOST = process.env.DB_HOST;
-const PORT = process.env.DB_PORT;
-const DB = process.env.DB_NAME;
-const USER = process.env.DB_USER;
-const PASSOWRD = process.env.DB_PASSWORD;
+const HOST = process.env.MONGOHOST;
+const PORT = process.env.MONGOPORT;
+const DB = process.env.MONGODB;
+const USER = process.env.MONGOUSER;
+const PASSOWRD = process.env.MONGOPASSWORD;
 
 try {
   await mongoose.connect(`mongodb://${USER}:${PASSOWRD}@${HOST}:${PORT}/${DB}?authSource=admin`);
